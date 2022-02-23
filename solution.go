5d6d4fd4fc1bfb1cbfc068bc6c5c50516f039d48
rosudavidg <rosudavidg@gmail.com>
2022-02-23 19:48:29 +0200

    Add solution

--- solution.go
+++ solution.go
@@ -1,12 +1,40 @@
 package square
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+import (
+	"math"
+)
 
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
+// Custom int type which defines a shape
+type ShapeSides int
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+// Constants that represents the number of shapes for a shape
+const (
+	SidesCircle   ShapeSides = 0
+	SidesTriangle ShapeSides = 3
+	SidesSquare   ShapeSides = 4
+)
+
+func calcCircleArea(radius float64) float64 {
+	return math.Pi * math.Pow(radius, 2)
+}
+
+func calcTriangleArea(sideLen float64) float64 {
+	return math.Pow(sideLen, 2) * math.Sqrt(3) / 4
+}
+
+func calcSquareArea(sideLen float64) float64 {
+	return math.Pow(sideLen, 2)
+}
+
+func CalcSquare(sideLen float64, sidesNum ShapeSides) float64 {
+	switch sidesNum {
+	case SidesCircle:
+		return calcCircleArea(sideLen)
+	case SidesTriangle:
+		return calcTriangleArea(sideLen)
+	case SidesSquare:
+		return calcSquareArea(sideLen)
+	}
+
+	panic("Unknown ShapeSides type")
 }
